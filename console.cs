aa1575ca5c83a2932e96e015b5198f9941a9e3f4
inderpreetsingh99 <43333262+inderpreetsingh99@users.noreply.github.com>
2018-09-21 10:14:26 +0530

    Add files via upload
    in class assignment 2 taken on 21.sep.2018

---
+++ console.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double maths = 60;
+            double physics = 65;
+            double chemistry = 70;
+
+            double average = (maths + physics + chemistry) / 3;
+            Console.WriteLine("average of "+maths+" , "+physics+" and "+chemistry+" is"+average);
+
+        }
+    }
+}
